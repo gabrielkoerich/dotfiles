beca73edb7fc64f6a789a3440d97ef4a60fff9b8
Gabriel Koerich <gabriel@algorit.com.br>
2016-07-07 02:25:23 -0300

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "brew.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        --exclude "screenshot.png" \
+        -avh --no-perms . ~;
+
+    . ~/.zshrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
