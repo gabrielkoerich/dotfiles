04e26ce6ea3c86de8b524f9307963b1af79ba6fb
Gabriel Koerich <gabriel@algorit.com.br>
2016-07-27 14:04:27 -0300

    Save screenshots on dropbox.

--- .macos.sh
+++ .macos.sh
@@ -213,8 +213,8 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+# Save screenshots to the Dropbox
+defaults write com.apple.screencapture location -string "${HOME}/Dropbox/Screenshots"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
