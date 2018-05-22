17dfff5033beb928f8ca4ac64ac9b18171f76837
evaldofelipe <contato@evaldofelipe.com>
2018-05-22 00:43:20 -0300

    Adjust random shit

--- private.sh
+++ private.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#this file copying other sh file where i've stored a sensitive file paths and sync via iclod
+#this file copying other sh file where i've stored a sensitive paths and sync via icloud
 
 cp $HOME/Documents/sync_configs/configs.sh .
 chmod +x configs.sh
