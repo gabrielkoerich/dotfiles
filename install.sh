f388a741ac7a8350e9cb81aebb96f3f74cca8703
Gabriel Koerich <gabriel@bulldesk.com.br>
2018-10-12 19:08:26 -0300

    Cleanup

--- install.sh
+++ install.sh
@@ -72,32 +72,6 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
 # Install other useful binaries.
 brew install ack
 brew install dark-mode
@@ -126,6 +100,7 @@ brew install innotop #innodb top cmd
 # brew install vbindiff
 # brew install webkit2png
 # brew install zopfli
+# brew install xpdf
 
 # Install brew cask and other apps
 brew tap phinze/homebrew-cask
@@ -136,6 +111,7 @@ brew cask install caffeine
 brew cask install spectacle
 brew cask install imageoptim
 brew cask install dropbox
+brew cask install github
 # brew cask install skype
 brew cask install sequel-pro
 # brew cask install rescuetime
@@ -143,11 +119,11 @@ brew cask install sublime-text
 brew cask install the-unarchiver
 brew cask install java
 brew cask install flux
-# brew cask install flycut
+brew cask install flycut
 brew cask install qlmarkdown # Add quicklook view for markdown files
-#brew cask install 0ad
 #brew cask install slack
-# brew cask install docker
+#brew cask install 0ad
+#brew cask install docker
 #brew cask install google-chrome
 
 # Add sublime command
