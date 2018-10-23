fbc6246e0dc5956715325a5dd167eb56a64348a7
Gabriel Koerich <gabriel@bulldesk.com.br>
2018-10-23 11:34:08 -0300

    Sync home/.

--- sync.sh
+++ sync.sh
@@ -5,18 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 # git pull origin master;
 
 function sync() {
-    rsync home/. ~ --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude ".gitignore" \
-        --exclude "config" \
-        --exclude "fonts" \
-        --exclude "fonts.sh" \
-        --exclude "install.sh" \
-        --exclude "macos.sh" \
-        --exclude "README.md" \
-        --exclude "ssh.sh" \
-        --exclude "sync.sh" \
-        -avh --no-perms;
+    rsync home/. ~ --exclude ".git/" --exclude ".DS_Store" -avh --no-perms;
 }
 
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
