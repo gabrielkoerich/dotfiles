8a818c73eafda07ff6c3e26f09fef5f55e1458f2
Gabriel Koerich <gabriel@bulldesk.com.br>
2018-10-25 01:48:59 -0300

    Install brew only if doesn't exist

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+set -e
+set -u
+set -o pipefail
+
+echo 'Starting...';
+
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || { 
+    echo >&2 "Installing Homebrew"; \
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; 
+}
 
 # Install command-line tools using Homebrew.
 
