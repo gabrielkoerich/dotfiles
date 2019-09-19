a50206231eb855c09819a5aa3251acdede09dae7
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-09-19 16:35:19 -0300

    Add to cron

--- cron.sh
+++ cron.sh
@@ -1,6 +1,10 @@
+# save current cron to a new file
 crontab -l > cron
+
 # echo new cron into cron file
-echo "0 10 * * * mackup backup --force >/dev/null 2>&1" > cron
+echo "0 10 * * * mackup backup --force >/tmp/stdout.log 2>/tmp/stderr.log" > cron
+echo "0 18 * * 5 sh /Users/gabriel/Projects/bulldesk/build/tag >/tmp/stdout.log 2>/tmp/stderr.log" > cron
+
 # install new cron file
 crontab cron
 rm cron
