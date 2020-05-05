a536730dd426d6c76bf31eed6734af65848b411a
Gabriel Koerich <gabriel@bulldesk.com.br>
2020-05-05 17:21:13 -0300

    Use fullpath

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 crontab -l > cron
 
 # echo new cron into cron file
-echo "0 10 * * * mackup backup --force >/tmp/stdout.log 2>/tmp/stderr.log" > cron
+echo "0 10 * * * /usr/local/bin/mackup backup --force >/tmp/stdout.log 2>/tmp/stderr.log" > cron
 echo "0 18 * * 5 sh /Users/gabriel/Projects/bulldesk/build/tag >/tmp/stdout.log 2>/tmp/stderr.log" > cron
 
 # install new cron file
