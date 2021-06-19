e0fe93c2adda60013bb8c3e7808552335963d82a
Gabriel Koerich <gabriel@bulldesk.com.br>
2021-06-19 15:09:54 -0300

    Install pcre2 and add link to php 8 to fix pecl builds on m1

--- install.sh
+++ install.sh
@@ -151,14 +151,18 @@ brew install redis
 brew install php
 brew install brew-php-switcher
 
-brew tap mongodb/brew
-brew install
-brew install mongodb-community
+#brew tap mongodb/brew
+#brew install
+#brew install mongodb-community
 
 # PHP Configs
 # cp ./config/php-memory-limits.ini /usr/local/etc/php/7.4/conf.d/php-memory-limits.ini
 cp ./config/php-memory-limits.ini /opt/homebrew/etc/php/7.4/conf.d/php-memory-limits.ini
 # cp ./config/psysh.php ~/.config/psysh/config.php
+
+# Fix pecl builds on mac m1
+brew install pcre2
+ln -s /opt/homebrew/Cellar/pcre2/10.37/include/pcre2.h /opt/homebrew/Cellar/php/"$(php -v | grep ^PHP | cut -d' ' -f2)"/include/php/ext/pcre/pcre2.h
 
 # Install Xdebug + Mongo via pecl
 pecl install xdebug
