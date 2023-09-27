29c22ba0b7f37da5c14dae185127d04eb66f47a4
Gabriel Koerich <gabrielmk@gmail.com>
2023-09-27 18:15:31 -0300

    powerline fonts

--- fonts.sh
+++ fonts.sh
@@ -1,2 +1,11 @@
 # Install Fonts
 cp fonts/*.ttf /Library/Fonts/ && echo "Fonts installed.";
+
+# clone
+git clone https://github.com/powerline/fonts.git powerline-fonts --depth=1
+# install
+cd powerline-fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf powerline-fonts
